080f2dfba6492c46ed26a0c18340bf4dba424a38
ted.song <ted.song@daumkakao.com>
2019-03-20 23:20:44 +0900

    MainTest

--- src/test/java/com/github/uuidcode/jmx/test/MainTest.java
+++ src/test/java/com/github/uuidcode/jmx/test/MainTest.java
@@ -1,6 +1,7 @@
 package com.github.uuidcode.jmx.test;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -17,12 +18,11 @@ import org.junit.Test;
 import org.slf4j.Logger;
 
 import com.github.uuidcode.util.CoreUtil;
+import com.sun.tools.attach.AttachNotSupportedException;
 import com.sun.tools.attach.VirtualMachine;
 import com.sun.tools.attach.VirtualMachineDescriptor;
 import com.sun.tools.attach.spi.AttachProvider;
 
-import static java.util.Optional.ofNullable;
-import static org.junit.Assert.*;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MainTest {
@@ -56,6 +56,50 @@ public class MainTest {
 
     @Test
     public void local() throws Exception {
+        MBeanServerConnection connection = getConnection();
+        ObjectName memory = new ObjectName("java.lang:type=Memory");
+        CompositeData compositeData = (CompositeData) connection.getAttribute(memory, "HeapMemoryUsage");
+        Long used = (Long) compositeData.get("used");
+
+        if (logger.isDebugEnabled()) {
+            logger.debug(">>> local used: {}", CoreUtil.toJson(used));
+        }
+    }
+
+    private MBeanServerConnection getConnection() throws Exception {
+        VirtualMachine virtualMachine = this.getVirtualMachine();
+        JMXServiceURL url = this.getJMXServiceURL(virtualMachine);
+        JMXConnector jmxConnector = JMXConnectorFactory.connect(url);
+        MBeanServerConnection connection = jmxConnector.getMBeanServerConnection();
+        this.printJMXMeta(connection);
+
+        return connection;
+    }
+
+    private void printJMXMeta(MBeanServerConnection connection) throws IOException {
+        Set<ObjectName> objectNameSet = connection.queryNames(null, null);
+
+        List<JMXMeta> jmxMetaList = objectNameSet.stream()
+            .map(JMXMeta::of)
+            .collect(Collectors.toList());
+
+        if (logger.isDebugEnabled()) {
+            logger.debug(">>> local jmxMetaList: {}", CoreUtil.toJson(jmxMetaList));
+        }
+    }
+
+    private JMXServiceURL getJMXServiceURL(VirtualMachine virtualMachine) throws Exception {
+        String agent = virtualMachine.getSystemProperties().getProperty("java.home") +
+            File.separator + "lib" + File.separator + "management-agent.jar";
+        virtualMachine.loadAgent(agent);
+
+        String localConnectorAddress = virtualMachine.getAgentProperties()
+            .getProperty("com.sun.management.jmxremote.localConnectorAddress");
+
+        return new JMXServiceURL(localConnectorAddress);
+    }
+
+    private VirtualMachine getVirtualMachine() throws AttachNotSupportedException, IOException {
         AttachProvider attachProvider = AttachProvider.providers().get(0);
 
         List<VirtualMachineDescriptor> virtualMachineDescriptorList = attachProvider.listVirtualMachines();
@@ -70,34 +114,6 @@ public class MainTest {
             .orElseThrow(() -> new RuntimeException("not exist"));
 
 
-        VirtualMachine virtualMachine = attachProvider.attachVirtualMachine(virtualMachineDescriptor);
-        String agent = virtualMachine.getSystemProperties().getProperty("java.home") +
-            File.separator + "lib" + File.separator + "management-agent.jar";
-        virtualMachine.loadAgent(agent);
-
-        String localConnectorAddress = virtualMachine.getAgentProperties()
-            .getProperty("com.sun.management.jmxremote.localConnectorAddress");
-
-        JMXServiceURL url = new JMXServiceURL(localConnectorAddress);
-        JMXConnector jmxConnector = JMXConnectorFactory.connect(url);
-        MBeanServerConnection connection = jmxConnector.getMBeanServerConnection();
-
-        Set<ObjectName> objectNameSet = connection.queryNames(null, null);
-
-        List<JMXMeta> jmxMetaList = objectNameSet.stream()
-            .map(JMXMeta::of)
-            .collect(Collectors.toList());
-
-        if (logger.isDebugEnabled()) {
-            logger.debug(">>> local jmxMetaList: {}", CoreUtil.toJson(jmxMetaList));
-        }
-
-        ObjectName memory = new ObjectName("java.lang:type=Memory");
-        CompositeData cd = (CompositeData) connection.getAttribute(memory, "HeapMemoryUsage");
-        Long used = (Long) cd.get("used");
-
-        if (logger.isDebugEnabled()) {
-            logger.debug(">>> local used: {}", CoreUtil.toJson(used));
-        }
+        return attachProvider.attachVirtualMachine(virtualMachineDescriptor);
     }
 }
